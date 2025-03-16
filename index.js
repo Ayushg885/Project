06ce7e6eaecf0406bed3b2815ef5c6eb041e4d48
Ayushg885 <147439228+Ayushg885@users.noreply.github.com>
2025-03-17 01:43:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-function toggleMenu() {
-    var menu = document.getElementById("draw-menu");
-    menu.classList.toggle("active");
-}
+let check='0'
+var menu = document.querySelector(".menu-btn");
+menu.addEventListener("click",function(){
+    console.log(`clickd,${check}`)
+    if(check==='0'){
+        document.querySelector('#draw-menu').style.display="block";
+        check='1'}
+    else{
+        document.querySelector('#draw-menu').style.display="none";
+        check='0'}
+})
